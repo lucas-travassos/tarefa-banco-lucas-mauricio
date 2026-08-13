ae35137de66c66a9b56baa4795400c4353db7d7a
lucas-travassos <lucasstravassos19@gmail.com>
2026-08-13 20:34:13 -0300

    feat: desenvolvido bloco 1 de consultas

--- consultas_nexashop.sql
+++ consultas_nexashop.sql
@@ -14,5 +14,43 @@ SELECT 'avaliacoes' AS tabela, COUNT(*) AS total FROM avaliacoes;
 
 /*Bloco 1 - Reconhecimento do banco*/
 
-SELECT * FROM clientes
+	/*Tarefa 1.1 — Primeiro contato com os dados*/
 
+		-- Listagem de 10 registros com todas as colunas da tabela clientes
+		SELECT * FROM clientes LIMIT 10;
+
+		-- Listagem de 10 registros com todas as colunas da tabela produtos
+		SELECT * FROM produtos LIMIT 10;
+
+		-- Listagem de 10 registros com todas as colunas da tabela pedidos
+		SELECT * FROM pedidos LIMIT 10;
+
+		-- Listagem de 10 registros com todas as colunas da tabela avaliacoes
+		SELECT * FROM avaliacoes LIMIT 10;
+
+	/*Tarefa 1.2 — Catálogo de produtos para o marketing*/
+
+		-- Listagem limpa do catálogo de produto, sem as colunas id e ativo
+		SELECT
+			nome as nome_produto,
+			categoria,
+			marca,
+			preco as "Valor (R$)",
+			estoque
+		FROM produtos;
+
+	/*Tarefa 1.3 — Quantas categorias a loja realmente vende*/
+
+		-- Listagem de todas as categorias dos produtos
+		SELECT DISTINCT(categoria) as "Lista de categorias"
+		FROM produtos;
+	
+	/*Tarefa 1.4 — Formas de pagamento e canais de venda aceitos*/
+
+		-- Listagem de todas as formas de pagamento dos pedidos
+		SELECT DISTINCT(forma_pagamento) as "Lista formas de pagamento"
+		FROM pedidos;
+
+		-- Listagem de todos os canais de venda dos pedidos
+		SELECT DISTINCT(canal_venda) as "Lista canais de venda"
+		FROM pedidos;
